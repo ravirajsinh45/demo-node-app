b014558803727203f9509d83b52a23a4c7b3e84a
Ravirajsinh Dabhi <59473511+ravirajsinh45@users.noreply.github.com>
2025-05-28 12:30:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Accept connections from any IP address
+const HOST = '0.0.0.0';
+
 app.get('/', (req, res) => {
-res.send('Hello from Node.js running on PM2!');
+  res.send('Hello from Node.js running on PM2!');
 });
 
-app.listen(PORT, () => {
-console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
